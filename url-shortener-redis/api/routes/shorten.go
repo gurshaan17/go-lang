c1d3c0c7d90102ad4ec653b4331709ce816f236f
gurshaan17 <gurshaansinghd@gmail.com>
2024-11-06 22:44:08 +0530

    shortenURL helpers made

--- url-shortener-redis/api/routes/shorten.go
+++ url-shortener-redis/api/routes/shorten.go
@@ -1,6 +1,10 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +18,22 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateLimiting   int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "can't parse json"})
+	}
+	//implement rate limit & check if input is an actual URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(400).JSON(fiber.Map{"error": "invalid URL"})
+	}
+	// check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(503).JSON(fiber.Map{"error": "you can't access it"})
+	}
+	// enforce https, ssl
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
