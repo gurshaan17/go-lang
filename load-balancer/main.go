3ba17ba84010e7e74f96263ba8f63366d94368e2
gurshaan17 <gurshaansinghd@gmail.com>
2024-11-10 20:00:13 +0530

    load balancer started

--- load-balancer/main.go
+++ load-balancer/main.go
@@ -1,9 +1,33 @@
 package main
 
-import(
-
+import (
+	"fmt"
+	"net/http/httputil"
+	"net/url"
+	"os"
 )
 
+type simpleServer struct{
+	addr	string
+	proxy   *httputil.ReverseProxy
+}
+
+func newSimpleServer(addr string) *simpleServer{
+	serverURL, err := url.Parse(addr)
+	handleErr(err)
+	return &simpleServer{
+		addr: addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverURL),
+	}
+}
+
+func handleErr(err error){
+	if err != nil{
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+}
+
 func main(){
-	
+
 }
